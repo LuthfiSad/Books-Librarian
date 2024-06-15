1c4f3119bbd08a01911c26d9c0d68f2c487c5c24
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-15 19:50:33 +0700

    perubahan title di app.jsx

--- src/entries/App.jsx
+++ src/entries/App.jsx
@@ -2,7 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "@/core/utils/routes.jsx";
 
 function App() {
-  document.title = "LibrarianApp";
   return (
     <>
       <RouterProvider router={router} />
