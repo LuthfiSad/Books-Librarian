83a2334f29d4d4dca8a6faecd76f0540dfead283
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-15 21:27:23 +0700

    bug  pada vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
   envPrefix: "LIB_",
   resolve: {
@@ -12,4 +15,4 @@ export default defineConfig({
       '@/features': path.resolve(__dirname, './src/features'),
     }
   }
-})
+});
